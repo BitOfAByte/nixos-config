37b2eb2bf95419c8c1b78d16e5b9c02ca9f3028f
BitOfAByte <hopetobylol@gmail.com>
2024-05-19 17:07:30 +0200

    First commit

---
+++ home.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "toby";
+  home.homeDirectory = "/home/toby";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+
+  ];
+
+
+  #Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/deekahy/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+
+  programs = {
+    nushell = { enable = true;
+# The config.nu can be anywhere you want if you like to edit your Nushell with Nu
+      configFile.source = ./home/toby/.config/nushell/config.nu;
+# for editing directly to config.nu 
+      extraConfig = ''
+        let carapace_completer = {|spans|
+          carapace $spans.0 nushell $spans | from json
+        }
+      $env.config = {
+show_banner: false,
+             completions: {
+case_sensitive: false # case-sensitive completions
+                  quick: true    # set to false to prevent auto-selecting completions
+                  partial: true    # set to false to prevent partial filling of the prompt
+                  algorithm: "fuzzy"    # prefix or fuzzy
+                  external: {
+# set to false to prevent nushell looking into $env.PATH to find more suggestions
+enable: true 
+# set to lower can improve completion performance at the cost of omitting some options
+          max_results: 100 
+          completer: $carapace_completer # check 'carapace_completer' 
+                  }
+             }
+      } 
+      $env.PATH = ($env.PATH | 
+          split row (char esep) |
+          prepend /home/myuser/.apps |
+          append /usr/bin/env
+          )
+        '';
+      shellAliases = {
+        vi = "nvim";
+        vim = "nvim";
+# nano = "hx";
+        cd = "z";
+      };
+    };  
+    carapace.enable = true;
+    carapace.enableNushellIntegration = true;
+
+    starship = { enable = true;
+      settings = {
+        add_newline = true;
+        character = { 
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+      };
+    };
+  };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
+
